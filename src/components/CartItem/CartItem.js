0d963a02796e8dd9b605c9041da5bd25bcaf7a05
ellen-ls <ellen.l.s1996@gmail.com>
2023-07-11 22:13:03 -0300

    atualizaçao

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { BsCartX } from 'react-icons/bs'
 import './CartItem.css'
 import formatCurrency from '../../utils/formatCurrency'
@@ -10,6 +10,11 @@ const CartItem = ({data}) => {
 
     const{cartItems, setCartItems} = useContext(AppContext)
     const {id, thumbnail, title, price} = data
+    const [counter, setCount] = useState(0);
+ 
+    const add = () => setCount(counter + 1);
+    const sub = () => setCount(counter - 1);
+
 
     const handleRemoveItem = () =>{
         const updatedItems = cartItems.filter((item)=>item.id !== id) 
@@ -26,9 +31,9 @@ const CartItem = ({data}) => {
     <div className='cart-item-content'>
         <h3 className='cart-item-title'>{title}</h3>
         <div className='cart-item-quanty'>
-                    <button className='button__quanty-item'><CiCircleMinus /></button>
-                    <span className='cart-item-number'>1</span>
-                    <button className='button__quanty-item'><CiCirclePlus /></button>
+                    <button className='button__quanty-item' onClick={sub}><CiCircleMinus /></button>
+                    <span className='cart-item-number'>{counter +1}</span>
+                    <button className='button__quanty-item' onClick={add}><CiCirclePlus /></button>
                 </div>
         <h3 className='cart-item-price'>{formatCurrency(price, 'BRL')}</h3>
         <button 
